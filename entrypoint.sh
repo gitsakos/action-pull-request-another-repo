b63e8ef9b1877b2df1438f204bc3a11a7181f8a0
bill mitsakos <bmitsakos@gmail.com>
2025-01-10 09:33:11 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,6 +42,7 @@ echo "Adding git commit"
 git add .
 if git status | grep -q "Changes to be committed"
 then
+  git pull
   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
   echo "Pushing git commit"
   git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
